1229274effef95e1da8999faf5642fe9b828ab1d
PavanKumarV741 <pavankumarv741@gmail.com>
2022-10-09 10:07:54 +0530

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path=require("path")
 const dataModel = require("./schema")
 const cors=require("cors")
 const multer=require("multer")
+require("dotenv").config()
 
 const app=express()
 
@@ -27,7 +28,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors())
 
-app.listen(5000 || PORT,(err)=>{
+const port=process.env.PORT || 5000
+
+app.listen(port,(err)=>{
     if(!err){
         console.log("server is running at port 5000")
     }
